f946b5361d4e7c9d18136fdbcbe340d83c330e68
dunkeng <liang_jinpeng@hotmail.com>
2019-12-19 12:12:50 +0800

    Create customize_wrt1200ac.sh

---
+++ customize_wrt1200ac.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# 原作者: eSirPlayground
+# 修改者：dunkeng
+#=================================================
+#1. Modify default IP
+#sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
